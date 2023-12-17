a51a8ae6b017b179af0eb0b4dbec0c0df827a24e
Antonio Guerriere <antonioguerriere@gmail.com>
2023-12-17 23:35:41 +0000

    wip header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,15 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      {/* Header */}
+      <div className="text-white h-screen bg-[url('/images/header.webp')] bg-cover bg-no-repeat bg-center">
+        <div className='bg-greenOverlay h-full'>
+          <h1 className='text-5xl font-bold p-5 leading-normal md:text-7xl'>
+            <span className='text-gold'>Family-owned</span> and operated optometry practice with over 40 years of experience.
+          </h1>
+        </div>
+        <div className=''>Learn more</div>
+      </div>
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,11 +11,13 @@ const config: Config = {
       colors: {
         grey1: '#737373',
         gold: '#D9B166',
+        greenOverlay: 'rgba(10, 64, 48, 0.85)',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          
       },
     },
   },
