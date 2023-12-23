e8509683068343cdfa8a5079275910429be14f3e
Antonio Guerriere <antonioguerriere@gmail.com>
2023-12-23 20:55:40 +0000

    Revert "installed codemod for legacy image component in nextjs"
    This reverts commit 0582740f60a5a4785bac69629738ce1a97c69dd0.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image"
+import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import GoldTitle from '@/components/GoldTitle'
 import LearnMore from '@/components/LearnMore'
