ca305e033d965f7c9322b2c377b7291fde5edae4
Antonio Guerriere <antonioguerriere@gmail.com>
2023-12-26 00:25:07 +0000

    completed our team section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -48,6 +48,47 @@ export default function About() {
             <div className="text-center text-teal-950 text-base lg:text-xl xl:text-2xl font-light font-['Source Sans Pro']">Support our local community and environment by participating in various charities and causes.</div>
           </div>
       </div>
+      {/* Section Team */}
+      <div>
+        <div className="mb-5">
+          <GoldTitle text="Meet the team" />
+        </div>
+        <H2Title title="Our Team"/>
+        <p className="text-base md:text-2xl font-light mb-16">We have a qualified and friendly team of optometrists and optical assistants who are ready to assist you with your needs and queries. Here are some of the members of our team:</p>
+        <div className="flex flex-col sm:flex-row border mb-10">
+          <div className="bg-green1 min-h-[420px] md:1/3 aspect-square"></div>
+          <div className="lg:ml-20 ml-5 flex flex-col justify-center">
+            <h4 className="text-xl md:text-3xl font-light">Tony Watson,</h4>
+            <h6 className="text-gold font-light mb-7">Owner and Optometrist</h6>
+            <p className="text-grey1 lg:w-3/4 text-base text-2xl">Tony is the founder and owner of Watsons Opticians, as well as a qualified and experienced optometrist. He has a passion for eye care and a vision for excellence. He oversees the daily operations of the practice and ensures that every customer receives the best service and care possible. He says: “I get a lot of satisfaction from what I do and I love seeing the smiles on our customers’ faces when they find the perfect pair of glasses or contact lenses. I am proud of our team and our reputation in the community”.</p>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row border mb-10">
+          <div className="lg:ml-20 ml-5 flex flex-col justify-center order-2">
+            <h4 className="text-xl md:text-3xl font-light">Valerie Watson, ,</h4>
+            <h6 className="text-gold font-light mb-7">Co-owner and Optical Clinical Assistant</h6>
+            <p className="text-grey1 lg:w-3/4 text-base text-2xl">Valerie is the co-owner and Optical Clinical Assistant of Watsons Opticians. She is also Tony’s wife and partner in life and business. As buyer for the business she strives to provide a wide range of products to meet our customers’ needs. She says: “I enjoy helping our customers look and feel great with their glasses or contact lenses. I also love working with my husband and our amazing staff. We are like a family here at Watsons Opticians”.</p>
+          </div>
+          <div className="bg-green1 min-h-[420px] md:1/3 aspect-square order-1"></div>
+        </div>
+        <div className="flex flex-col sm:flex-row border mb-10">
+          <div className="bg-green1 min-h-[420px] md:1/3 aspect-square"></div>
+          <div className="lg:ml-20 ml-5 flex flex-col justify-center">
+            <h4 className="text-xl md:text-3xl font-light">Christine Mc Collum, ,</h4>
+            <h6 className="text-gold font-light mb-7">Senior Optical Assistant</h6>
+            <p className="text-grey1 lg:w-3/4 text-base text-2xl">Christine handles the bookings, the payments and also cuts and fits lenses to frames in our glazing lab. She says: “Watsons Opticians is a family friendly practice and I enjoy helping to contribute to its growth and success. I like to interact with our customers and to spread the word about our products and services. I like the fact that we are a progressive practice and we are all constantly learning new techniques and treatments”.</p>
+          </div>
+        </div>
+        <div className="flex flex-col sm:flex-row border mb-10">
+          <div className="lg:ml-20 ml-5 flex flex-col justify-center order-2">
+            <h4 className="text-xl md:text-3xl font-light">Katrina Watt,</h4>
+            <h6 className="text-gold font-light mb-7">Optical Assistant</h6>
+            <p className="text-grey1 lg:w-3/4 text-base text-2xl">Christine handles the bookings, the payments and also cuts and fits lenses to frames in our glazing lab. She says: “Watsons Opticians is a family friendly practice and I enjoy helping to contribute to its growth and success. I like to interact with our customers and to spread the word about our products and services. I like the fact that we are a progressive practice and we are all constantly learning new techniques and treatments”.</p>
+          </div>
+          <div className="bg-green1 min-h-[420px] md:1/3 aspect-square order-1"></div>
+
+        </div>
+      </div>
       <Footer />
     </>
   )
