6e7365398e01361ae1e0bd637f8be4250afffb38
Antonio Guerriere <antonioguerriere@gmail.com>
2023-12-30 16:06:44 +0100

    wip section 2 frames

--- src/app/frames/page.tsx
+++ src/app/frames/page.tsx
@@ -6,23 +6,24 @@ import GoldTitle from "@/components/GoldTitle"
 import H2Title from "@/components/H2Title"
 
 
-export default function Frames(){
-  return(
+
+export default function Frames() {
+  return (
     <>
       <Navbar />
-      <Header title="Eye Wear" image="/images/homepage/10.webp" percentage="30"/>
+      <Header title="Eye Wear" image="/images/homepage/10.webp" percentage="30" />
       {/* Section 1 */}
-      <div className="flex mt-10 mb-10">
-        <div className="w-full sm:w-1/2 min-h-[200px] h-fill hidden sm:block relative">
+      <div className="flex flex-col sm:flex-row mt-10 mb-10">
+        <div className="w-full sm:w-1/2 sm:order-1 mt-7 sm:mt-0 order-2 min-h-[200px] h-fill relative">
           <Image
-              src="/images/homepage/1.webp"
-              fill={true} 
-              alt="Picture of a patient undergoing an eye test"
-              sizes="(max-width: 768px) 50vw, (max-width: 1200px) 40vw, 33vw"
-              className="object-cover object-center"
-            />
+            src="/images/homepage/1.webp"
+            fill={true}
+            alt="Picture of a patient undergoing an eye test"
+            sizes="(max-width: 768px) 50vw, (max-width: 1200px) 40vw, 33vw"
+            className="object-cover object-center"
+          />
         </div>
-        <div className="sm:w-1/2 sm:ml-10">
+        <div className="sm:w-1/2 sm:ml-10 order-1 sm:order-2">
           <GoldTitle text="Eyewear" />
           <br />
           <H2Title title="Frames" />
@@ -31,6 +32,66 @@ export default function Frames(){
 
       </div>
       {/* Section 2 */}
+      <GoldTitle text="Brands" />
+      <div className="overflow-auto whitespace-nowrap flex pb-7 pt-7">
+        <Image
+          src="/images/brands/Picture_1.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_2.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_3.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_4.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_5.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_6.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+        <Image
+          src="/images/brands/Picture_7.webp"
+          width={400}
+          height={100}
+          alt="Picture of a patient undergoing an eye test"
+          className='pr-2'
+          style={{ objectFit: 'contain' }}
+        />
+
+      </div>
       {/* Section 3 */}
       {/* Section 4 */}
       {/* Section 5 */}
