1029660c5670a6389c20aafa085911a6412cbe92
Antonio Guerriere <antonioguerriere@gmail.com>
2024-01-02 21:14:32 +0100

    section 3 contact lenses completed

--- src/app/contact_lenses/page.tsx
+++ src/app/contact_lenses/page.tsx
@@ -58,8 +58,8 @@ export default function ContactLenses() {
             </div >
             {/* Right */}
             <div className="sm:w-1/2 p-5 sm:p-10">
-              <div><Checkmark text="More economical than daily lenses, but require more care and cleaning" /></div>
-              <div><Checkmark text="Suitable for people who wear contact lenses regularly and have a stable prescription" /></div>
+              <div><Checkmark text="More customisable than fortnightly or monthly lenses, but require the most care and cleaning" /></div>
+              <div><Checkmark text="Suitable for people who have complex prescriptions or special eye conditions" /></div>
             </div>
           </div>
           {/* Row 4*/}
@@ -84,13 +84,51 @@ export default function ContactLenses() {
             {/* Right */}
             <div className="sm:w-1/2 p-5 sm:p-10">
               <div><Checkmark text="Also known as ortho-k or corneal refractive therapy lenses" /></div>
-              <div><Checkmark text="Suitable for people who wear contact lenses regularly and have a stable prescription" /></div>
+              <div><Checkmark text="More innovative and convenient than other types of lenses" /></div>
             </div>
           </div>
         </div>
       </div>
       {/* Section 3 */}
+      {/* Outer Container for the green background */}
+      <div className="bg-green1 -ml-7 -mr-7 md:-ml-20 md:-mr-20 mb-10 p-7 md:p-20">
+        <GoldTitle text="Orthokeratology lenses" />
+        <br />
+        <h2 className="text-white text-xl sm:text-[40px] font-normal">Ortho-K lenses</h2>
+        <br />
+        {/* Flex container for left and right side */}
+        <div className="flex flex-col sm:flex-row justify-between">
+          {/* Left */}
+          <div className="sm:w-2/5">
+            <p className="text-white text-base sm:text-2xl font-light">Ortho-K contact lenses are designed to be worn overnight while you sleep rather than during the day. They work by gently and gradually reshaping the surface of the eye meaning that when you take them out in the morning, you should be able to see clearly throughout the day, without the need for any other lenses or glasses. They are also used for Myopia management</p>
+            <br /><br />
+            <h2 className="text-orange-300 text-xl sm:text-[40px] font-normal mb-2">20 years</h2>
+            <p className="text-white text-base sm:text-2xl font-normal">of experience fitting Ortho-K lenses</p>
+            <br /><br />
+            <p className="text-white text-base sm:text-2xl font-light">They’re suitable for those who are short sighted with a prescription of less than -5.00 with low or no astigmatism. Unfortunately they’re not suitable for patients who are long sighted.</p>
+          </div>
+          {/* Right */}
+          <div className="sm:w-2/5">
+            <p className="text-white text-base sm:text-2xl font-normal ">Why choose Ortho-K lenses?</p>
+            <br />
+            <p className="text-white text-base sm:text-2xl font-light">Ortho-K are ideal for people who:</p>
+            <br />
+            <ul className="list-none text-white text-base sm:text-2xl font-light">
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Want clear natural vision</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Want to be free from lenses for sports and outdoor activities for example water sports and boxing.</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Do not want to undergo laser surgery</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Children as it can slow down the progression of shortsightedness</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Find it hard to wear contact lenses all day without discomfort</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Suffer from hay-fever</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Work in a very dusty, dry or smoky environment</li>
+              <li className="flex flex-row"><div className="w-[15px] h-[15px] bg-orange-300 rounded-full mt-1 mr-5 aspect-square mb-3"></div>Find spectacles and conventional lenses just a plain nuisance</li>
+              
+            </ul>
+          </div>
+        </div>
+      </div>
       {/* Section 4 */}
+      
       <Footer />
     </>
   )
