85c6d5abf77378cb0a94fbd9b7e98401562c017c
Antonio Guerriere <antonioguerriere@gmail.com>
2024-01-21 18:26:41 +0000

    changed locations order in footer as per client request

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,45 +3,48 @@ import Link from "next/link"
 
 
 export default function Footer() {
-  return(
+  return (
     <>
       <div className="flex flex-row items-center justify-evenly bg-green1 -ml-7 -mr-7 md:-ml-20 md:-mr-20 text-white min-h-[290px] p-7">
-          <div className="min-h-[290px]">
-            <Image
-              src="/images/white-logo.webp"
-              width={163}
-              height={80}
-              alt="Watsons Opticians Logo"
-              className="pl-1 pr-2 md:p-0"
-            />
+        <div className="min-h-[290px]">
+          <Image
+            src="/images/white-logo.webp"
+            width={163}
+            height={80}
+            alt="Watsons Opticians Logo"
+            className="pl-1 pr-2 md:p-0"
+          />
+        </div>
+        <div className="flex flex-col justify-between min-h-[290px]">
+          <div className="flex flex-col">
+            <Link href="/">Home</Link>
+            <Link href="/about">About Us</Link>
           </div>
-          <div className="flex flex-col justify-between min-h-[290px]">
-            <div className="flex flex-col">
-              <Link href="/">Home</Link>
-              <Link href="/about">About Us</Link>
-            </div>
-            <div>
+          <div>
             <h5>Locations</h5>
-            <p className="text-xs text-grey2"> <br />
+            <h5>Ballycastle</h5>
+            <p className="text-xs text-grey2">
+              41 Ann St, Ballycastle, BT54 6AA <br />
+              Call us on 028 2076 2431
+            </p>
+            <h5>Portrush</h5>
+            <p className="text-xs text-grey2">
               10 Main St, Portrush, BT56 8BL <br />
               Call us on 028 7082 3677
             </p>
-            <p className="text-xs text-grey2">
-              41 Ann St, Ballycastle, BT54 6AA <br />
-              Call us on 028 2076 2431 
-            </p>
+
           </div>
+        </div>
+        <div className="flex flex-col justify-between min-h-[290px]">
+          <div className="flex flex-col">
+            <Link href="/eye_tests">Eye Tests</Link>
+            <Link href="/frames">Eye Wear</Link>
+            <Link href="/contact_lenses">Contact Lenses</Link>
+            <Link href="/eye_conditions">Eye Conditions</Link>
           </div>
-          <div className="flex flex-col justify-between min-h-[290px]">
-            <div className="flex flex-col">
-              <Link href="/eye_tests">Eye Tests</Link>
-              <Link href="/frames">Eye Wear</Link>
-              <Link href="/contact_lenses">Contact Lenses</Link>
-              <Link href="/eye_conditions">Eye Conditions</Link>
-            </div>
-            <div>
+          <div>
             <h5>Opening Hours</h5> <br />
-            <p className="text-xs text-grey2">         
+            <p className="text-xs text-grey2">
               Monday - Thursday: 9:00 - 17:00 <br />
               Friday: 9:00 - 15:00 <br />
               Saturday: Closed <br />
