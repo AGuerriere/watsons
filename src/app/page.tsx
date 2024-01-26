f0c8a3658422824d8771b3de72c8cfc7a4f0498d
Antonio Guerriere <antonioguerriere@gmail.com>
2024-01-26 00:36:11 +0000

    fixed spacing homepage as per designer specs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,33 +24,36 @@ export default function Home() {
         </div>
       </div>
       {/* Section 1*/}
-      <div className='flex flex-col lg:flex-row mt-20 pb-20'>
-        <div className="flex order-2 lg:order-1 justify-center lg:justify-end w-full lg:w-2/5 relative min-h-[250px] lg:min-h-[500px]">
-          <Image
-            src="/images/homepage/6.webp"
-            fill
-            sizes="100%"
-            alt="Picture of an eye test tool"
-            className='w-auto object-cover'
-          />
-        </div>
-        <br /><br />
-        <div className='flex w-full lg:w-2/5 order-1 lg:order-2 flex-col justify-between lg:ml-20'>
-          <div className='mb-10'>
-            <GoldTitle text="At Watsons" />
+      <div className='flex justify-center w-full'>
+        <div className='flex flex-col lg:flex-row mt-20 pb-20 2xl:w-[1600px]'>
+          <div className="flex order-2 lg:order-1 justify-center lg:justify-end w-full lg:w-2/5 relative min-h-[250px] lg:min-h-[500px]">
+            <Image
+              src="/images/homepage/6.webp"
+              fill
+              sizes="100%"
+              alt="Picture of an eye test tool"
+              className='w-auto object-cover'
+            />
           </div>
-          <H2Title title="We’re passionate about making eyecare accessible" />
-          <p className='text-base text-base md:text-2xl font-light leading-normal'>
-            See better and look great with our wide range of glasses, contact lenses and sunglasses from leading brands.
-            <br /><br />
-            We offer comprehensive eye tests, diagnosis and treatment for various eye conditions using the latest technology and equipment. We are the only opticians in Northern Ireland offering Repeated Low Level Red Light Therapy to slow down progression of myopia in children.
-          </p>
           <br /><br />
-          <LearnMore link="/about" target="_self" text="Learn more about us" />
+          <div className='flex w-full lg:w-2/5 order-1 lg:order-2 flex-col justify-between lg:ml-20'>
+            <div className='mb-10'>
+              <GoldTitle text="At Watsons" />
+            </div>
+            <H2Title title="We’re passionate about making eyecare accessible" />
+            <p className='text-base text-base md:text-2xl font-light leading-normal'>
+              See better and look great with our wide range of glasses, contact lenses and sunglasses from leading brands.
+              <br /><br />
+              We offer comprehensive eye tests, diagnosis and treatment for various eye conditions using the latest technology and equipment. We are the only opticians in Northern Ireland offering Repeated Low Level Red Light Therapy to slow down progression of myopia in children.
+            </p>
+            <br /><br />
+            <LearnMore link="/about" target="_self" text="Learn more about us" />
+          </div>
         </div>
       </div>
       {/* Section 2*/}
-      <div>
+      <div className='flex justify-center w-full'>
+        <div className='2xl:w-[1600px]'>
         <H2Title title="Our Products and Services" />
         <div className='grid grid-cols-1 min-[1335px]:grid-cols-2 min-[1910px]:grid-cols-3 gap-4'>
           <Card title="Eye Tests" target="_self" link="/eye_tests" img="/images/homepage/3.webp" paragraph="An eye test is not just about finding out if you need glasses or contact lenses. It is a vital health check for your eyes." />
@@ -60,11 +63,13 @@ export default function Home() {
           <Card title="Dry eye clinic" target="_self" link="/dry_eye" img="/images/homepage/7.webp" paragraph="With the help of our Dry Eye clinic we can recommend the best tailored plan to minimise any discomfort." />
           <Card title="Myopia management" target="_self" link="/myopia_management" img="/images/homepage/12.webp" paragraph="Repeated Low Level Red Light (RLRL) therapy designed to slow down the progression of myopia." />
         </div>
+        </div>
       </div>
 
+
       {/* Section 3 */}
-      <div className='bg-green1 flex flex-col md:flex-row text-white -ml-7 -mr-7 md:-ml-20 md:-mr-20 md:pl-20 w-[100vw] mb-10 '>
-        <div className='w-full md:w-2/3 pt-10 pl-7'>
+      <div className='bg-green1 flex flex-col relative justify-end md:flex-row text-white -ml-7 -mr-7 md:-ml-20 md:-mr-20 md:pl-20 w-[100vw] mb-10 '>
+        <div className='w-full md:w-2/3 pt-10 pl-7 md:pl-0 2xl:w-[800px]'>
           <GoldTitle text='NHS PEARS' />
           <br />
           <h2 className='text-white text-2xl xl:text-5xl  font-normal pb-10'>Free NHS services</h2>
@@ -75,7 +80,7 @@ export default function Home() {
           </div>
           <br /><br />
         </div>
-        <div className="flex justify-center md:justify-end w-full md:w-2/3 relative min-h-[400px]">
+        <div className="flex justify-center md:justify-end w-full md:w-2/3 2xl:w-[50vw] relative min-h-[400px] ">
           <Image
             src="/images/homepage/8.webp"
             fill
@@ -86,7 +91,8 @@ export default function Home() {
         </div>
       </div>
       {/* Form Section */}
-      <div id="contact-us" className="mb-10">
+      <div className='flex justify-center w-full'>
+      <div id="contact-us" className="mb-10 2xl:w-[1600px]">
         <H2Title title="Book your appointment today!" />
         <div className='flex lg:flex-row flex-col'>
           <div className='w-full md:w-1/2'>
@@ -102,7 +108,7 @@ export default function Home() {
               <input type="text" name="subject" id="subject" placeholder="Subject" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
               <input type="text" name="text" id="text" placeholder="Type your message here" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
               <div className='flex'>
-                <div className='flex'>
+                <div className='flex justify-center items-center'>
                   <input type="radio" id="ballycastle" name="location" value="Ballycastle" required />
                   <p className="ml-1 mr-2">Ballycastle</p>
                 </div>
@@ -114,7 +120,7 @@ export default function Home() {
               <button className='mt-4'><GoldButton text='Submit  '></GoldButton></button>
             </form>
           </div>
-          <div className='flex flex-col justify-between lg:ml-10 min-h-[500px]'>
+          <div className='flex flex-col justify-between lg:ml-20 min-h-[500px]'>
             <GoldTitle text="Locations" />
             <h4 className='text-2xl font-normal text-black'>Ballycastle</h4>
             <p className='font-light'>
@@ -140,6 +146,7 @@ export default function Home() {
             </p>
           </div>
         </div>
+      </div>
       </div>
       <Footer />
 
