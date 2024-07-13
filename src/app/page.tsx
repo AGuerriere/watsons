2fa1d94afe3af955c3339510bada2b33910421aa
Antonio Guerriere <antonioguerriere@gmail.com>
2024-07-13 13:22:27 +0200

    changed the form to web3forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
 import GoldTitle from '@/components/GoldTitle'
@@ -7,8 +9,42 @@ import GoldButton from '@/components/GoldButton'
 import Footer from '@/components/Footer'
 import H2Title from '@/components/H2Title'
 import Link from 'next/link'
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitSuccessful, isSubmitting },
+  } = useForm({
+    mode: "onTouched",
+  });
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [message, setMessage] = useState("");
+
+  // Please update the Access Key in the .env
+  const apiKey = process.env.PUBLIC_ACCESS_KEY || "72eabd82-8da4-48ff-8ec9-26e5b706b153";
+
+  const { submit: onSubmit } = useWeb3Forms({
+    access_key: apiKey,
+    settings: {
+      from_name: "Watsons Opticians",
+      subject: "New Contact Message from your Website",
+    },
+    onSuccess: (msg, data) => {
+      setIsSuccess(true);
+      setMessage(msg);
+      reset();
+    },
+    onError: (msg, data) => {
+      setIsSuccess(false);
+      setMessage(msg);
+    },
+  });
+
   return (
     <>
       <Navbar />
@@ -54,15 +90,15 @@ export default function Home() {
       {/* Section 2*/}
       <div className='flex justify-center w-full'>
         <div className='2xl:w-[1600px]'>
-        <H2Title title="Our Products and Services" />
-        <div className='grid grid-cols-1 min-[1335px]:grid-cols-2 min-[1910px]:grid-cols-3 gap-4'>
-          <Card title="Eye Tests" target="_self" link="/eye_tests" img="/images/homepage/3.webp" paragraph="An eye test is not just about finding out if you need glasses or contact lenses. It is a vital health check for your eyes." />
-          <Card title="Eye Wear" target="_self" link="/frames" img="/images/homepage/1.webp" paragraph="We offer a wide selection to cater for different prescriptions, and lifestyle requirements." />
-          <Card title="Contact Lenses" target="_self" link="/contact_lenses" img="/images/homepage/2.webp" paragraph="If you prefer contact lenses, we can help you find the best fit and type for your eyes." />
-          <Card title="PEARS (NHS free service)" link="NI_PEARS" target="_self" img="/images/homepage/5.webp" paragraph="The NHS has launched a new free eye care service for people with minor eye conditions." />
-          <Card title="Dry eye clinic" target="_self" link="/dry_eye" img="/images/homepage/7.webp" paragraph="With the help of our Dry Eye clinic we can recommend the best tailored plan to minimise any discomfort." />
-          <Card title="Myopia management" target="_self" link="/myopia_management" img="/images/homepage/12.webp" paragraph="Repeated Low Level Red Light (RLRL) therapy designed to slow down the progression of myopia." />
-        </div>
+          <H2Title title="Our Products and Services" />
+          <div className='grid grid-cols-1 min-[1335px]:grid-cols-2 min-[1910px]:grid-cols-3 gap-4'>
+            <Card title="Eye Tests" target="_self" link="/eye_tests" img="/images/homepage/3.webp" paragraph="An eye test is not just about finding out if you need glasses or contact lenses. It is a vital health check for your eyes." />
+            <Card title="Eye Wear" target="_self" link="/frames" img="/images/homepage/1.webp" paragraph="We offer a wide selection to cater for different prescriptions, and lifestyle requirements." />
+            <Card title="Contact Lenses" target="_self" link="/contact_lenses" img="/images/homepage/2.webp" paragraph="If you prefer contact lenses, we can help you find the best fit and type for your eyes." />
+            <Card title="PEARS (NHS free service)" link="NI_PEARS" target="_self" img="/images/homepage/5.webp" paragraph="The NHS has launched a new free eye care service for people with minor eye conditions." />
+            <Card title="Dry eye clinic" target="_self" link="/dry_eye" img="/images/homepage/7.webp" paragraph="With the help of our Dry Eye clinic we can recommend the best tailored plan to minimise any discomfort." />
+            <Card title="Myopia management" target="_self" link="/myopia_management" img="/images/homepage/12.webp" paragraph="Repeated Low Level Red Light (RLRL) therapy designed to slow down the progression of myopia." />
+          </div>
         </div>
       </div>
 
@@ -92,61 +128,114 @@ export default function Home() {
       </div>
       {/* Form Section */}
       <div className='flex justify-center w-full'>
-      <div id="contact-us" className="mb-10 2xl:w-[1600px]">
-        <H2Title title="Book your appointment today!" />
-        <div className='flex lg:flex-row flex-col'>
-          <div className='w-full md:w-1/2'>
-            <GoldTitle text="Contact us" />
-            <br />
-            <h4 className="font-normal text-black text-base md:text-lg md:text-xl lg:text-3xl">Get in touch!</h4> <br />
-            <p className='text-grey1 text-base mb-2 md:text-xl lg:text-2xl font-light'>To arrange an appointment or register as a new customers please fill in the form below and we will get back to you within 48 hours: </p>
-            <br />
-            <form name="contact" method="POST" action="/success" data-netlify="true" className="flex flex-col mb-10">
-              <input type="text" name="name" id="name" placeholder="Name*" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
-              <input type="email" name="email" id="email" placeholder="Email*" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
-              <input type="tel" name="tel" id="tel" placeholder="Mobile Number*" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
-              <input type="text" name="subject" id="subject" placeholder="Subject*" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
-              <input type="text" name="text" id="text" placeholder="Type your message here*" className="bg-grey3 h-16 pl-5 rounded border mb-3 placeholder:text-grey4" required />
-              <div className='flex'>
-                <div className='flex justify-center items-center'>
-                  <input type="radio" id="ballycastle" name="location" value="Ballycastle" required />
-                  <p className="ml-1 mr-2">Ballycastle</p>
+        <div id="contact-us" className="mb-10 2xl:w-[1600px]">
+          <H2Title title="Book your appointment today!" />
+          <div className='flex lg:flex-row flex-col'>
+            <div className='w-full md:w-1/2'>
+              <GoldTitle text="Contact us" />
+              <br />
+              <h4 className="font-normal text-black text-base md:text-lg md:text-xl lg:text-3xl">Get in touch!</h4> <br />
+              <p className='text-grey1 text-base mb-2 md:text-xl lg:text-2xl font-light'>To arrange an appointment or register as a new customers please fill in the form below and we will get back to you within 48 hours: </p>
+              <br />
+              <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col mb-10">
+                <input
+                  {...register("botcheck")}
+                  type="checkbox"
+                  className="hidden"
+                  style={{ display: "none" }}></input>
+                <input
+                  type="hidden"
+                  {...register("redirect")}
+                  value="./success" />
+                <div className="mb-3">
+                  <input
+                    {...register("name", { required: true })}
+                    className="bg-grey3 h-16 pl-5 w-full rounded border mb-3 placeholder:text-grey4"
+                    placeholder="Enter your name*"
+                    type="text"
+                    id="yourname" />
+                </div>
+                <div className="mb-3">
+                  <input
+                    {...register("email", { required: true })}
+                    className="bg-grey3 h-16 pl-5 w-full rounded border mb-3 placeholder:text-grey4"
+                    placeholder="Email address*"
+                    type="email"
+                    id="youremail" />
+                </div>
+                <div className="mb-3">
+                  <input
+                    {...register("tel", { required: true })}
+                    className="bg-grey3 h-16 pl-5 w-full rounded border mb-3 placeholder:text-grey4"
+                    placeholder="Mobile Number*"
+                    type="tel"
+                    id="tel" />
+                </div>
+                <div className="mb-3">
+                  <input
+                    {...register("subject", { required: true })}
+                    className="bg-grey3 h-16 pl-5 w-full rounded border mb-3 placeholder:text-grey4"
+                    placeholder="Subject*"
+                    type="text"
+                    id="subject" />
+                </div>
+                <div className="mb-3">
+                  <textarea
+                    {...register("message", { required: true })}
+                    className="bg-grey3 h-16 pl-5 w-full rounded border mb-3 placeholder:text-grey4"
+                    placeholder="Type your message here*"
+                    id="message"></textarea>
                 </div>
                 <div className='flex'>
-                  <input type="radio" id="portrush" name="location" value="Portrush" required />
-                  <p className="ml-1">Portrush</p>
+                  <div className='flex justify-center items-center'>
+                    <input type="radio" id="ballycastle" name="location" value="Ballycastle" required />
+                    <p className="ml-1 mr-2">Ballycastle</p>
+                  </div>
+                  <div className='flex'>
+                    <input type="radio" id="portrush" name="location" value="Portrush" required />
+                    <p className="ml-1">Portrush</p>
+                  </div>
                 </div>
-              </div>
-              <button className='mt-4'><GoldButton text='Submit  '></GoldButton></button>
-            </form>
-          </div>
-          <div className='flex flex-col justify-between lg:ml-20 min-h-[500px]'>
-            <GoldTitle text="Locations" />
-            <h4 className='text-2xl font-normal text-black'>Ballycastle</h4>
-            <p className='font-light'>
-              41 Ann St, Ballycastle, BT54 6AA <br />
-              Call us on 028 2076 2431
-            </p>
-            <h5 className='text-black text-xl'>Opening Hours</h5>
-            <p className='font-light'>
-              Monday - Saturday: 9:00 - 17:30 <br />
-              Alternate Wednesday: Closed <br />
-              Sunday : Closed <br />
-            </p>
-            <h4 className='text-2xl font-normal text-black'>Portrush</h4>
-            <p className='font-light'>
-              10 Main St, Portrush, BT56 8BL <br />
-              Call us on 028 7082 3677
-            </p>
-            <h5 className='text-black text-xl'>Opening Hours</h5>
-            <p className='font-light'>
-              Monday - Tuesday, Thursday, Friday, Saturday: 9:00 - 17:30 <br />
-              Alternate Saturday: Closed <br />
-              Sunday : Closed <br />
-            </p>
+                <button className='mt-4'><GoldButton text='Submit  '></GoldButton></button>
+                {isSubmitSuccessful && isSuccess && (
+                  <div className="mt-3 text-sm text-center text-green-500">
+                    {message || "Success. Message sent successfully"}
+                  </div>
+                )}
+                {isSubmitSuccessful && !isSuccess && (
+                  <div className="mt-3 text-sm text-center text-red-500">
+                    {message || "Something went wrong. Please try later."}
+                  </div>
+                )}
+              </form>
+            </div>
+            <div className='flex flex-col lg:ml-20'>
+              <GoldTitle text="Locations"/>
+              <h4 className='text-2xl font-normal text-black mt-5'>Ballycastle</h4>
+              <p className='font-light mb-5'>
+                41 Ann St, Ballycastle, BT54 6AA <br />
+                Call us on 028 2076 2431
+              </p>
+              <h5 className='text-black text-xl'>Opening Hours</h5>
+              <p className='font-light mb-5'>
+                Monday - Saturday: 9:00 - 17:30 <br />
+                Alternate Wednesday: Closed <br />
+                Sunday : Closed <br />
+              </p>
+              <h4 className='text-2xl font-normal text-black'>Portrush</h4>
+              <p className='font-light mb-5'>
+                10 Main St, Portrush, BT56 8BL <br />
+                Call us on 028 7082 3677
+              </p>
+              <h5 className='text-black text-xl'>Opening Hours</h5>
+              <p className='font-light mb-5'>
+                Monday - Tuesday, Thursday, Friday, Saturday: 9:00 - 17:30 <br />
+                Alternate Saturday: Closed <br />
+                Sunday : Closed <br />
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <Footer />
 
